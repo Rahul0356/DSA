01e35b83f429fa848db9074aa08c908e83101fa7
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-10-21 20:54:07 +0530

    Day-10

---
+++ Day-10/count.js
@@ -0,0 +1,18 @@
+// write a function that returns the count of digits in a number
+
+  
+  
+  
+  function countDigits(n){
+let  count = 0;
+while(n>0){
+    n = Math.floor(n/10);
+    count++;
+}
+return count;
+}
+let num = 259;
+
+let result = countDigits(num);
+
+console.log(result)
