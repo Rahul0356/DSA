f1c2f307a73e85792e3e7c3527b7bed7b9af8724
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-11-23 11:52:30 +0530

    Day-21

--- Day-21/factorial.js
+++ Day-21/factorial.js
@@ -1,5 +1,11 @@
+// function fact(n){
+//     if(n==1) return 1;
+//     return n *fact(n-1);
+// }
+// console.log(fact(10));
+
 function fact(n){
     if(n==1) return 1;
-    return n *fact(n-1);
+    return n*fact(n-1);
 }
-console.log(fact(10));
+console.log(fact(5));
